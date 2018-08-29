348bcfa7dcbc806d917a60b1cdcf09259c6ba090
begimai <begimayapsamatova@gmail.com>
2018-08-29 16:59:40 +0600

    Don't really remember what have I done in these folder.

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class MyModel(models.Model):
-    #file will be uploaded to MEDIA_ROOT/uploads
+    # file will be uploaded to MEDIA_ROOT/uploads
     upload = models.FileField(upload_to='uploads/')
     # or...
     # file will be saved to MEDIA_ROOT/uploads/2015/01/30

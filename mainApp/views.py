5228e99f759ef9fb1e709c79e37e213d0787b469
begimai <begimayapsamatova@gmail.com>
2018-09-04 15:11:23 +0600

    added code to save data from forms, and tied views with models

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,20 +1,31 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.http import HttpResponse
-import datetime
-import youtube_dl
-from .forms import UsesDownloadWithYoutubelibrary
+from .forms import VideoConversionForm
+from .models import Video
 from .downloadsYoutube import getlinkdownloadNew
 
-def index (request):
-    if request.method == "POST":
-        tempForm = UsesDownloadWithYoutubelibrary(request.POST)
-        if tempForm.is_valid():
-            temp2 = UsesDownloadWithYoutubelibrary()
-            link = tempForm.cleaned_data['links']
-            mail_address = tempForm.cleaned_data['mail_address']
-            temp = getlinkdownloadNew(link)
 
-            return HttpResponse ("<h1>Thank You, link will be sent to Your e-mail</h1>")
+def index(request):
+    if request.method == "POST":
+        form = VideoConversionForm(request.POST)
+
+        if form.is_valid():
+            link = form.cleaned_data['links']
+            path, title = getlinkdownloadNew(link)
+            email = form.cleaned_data['mail_address']
+
+            video = Video(email=email, path=path, title=title)
+            video.save()
+
+            return HttpResponse("<h1>Thank You. Your download link for {} is /get/{}.</h1>".format(title, video.id))
     else:
-        useForm = UsesDownloadWithYoutubelibrary()
+        useForm = VideoConversionForm()
         return render (request,"mainApp/HomePage.html",{"form":useForm})
+
+
+def list(request):
+    return HttpResponse(list='title')
+
+
+def download(request):
+    return HttpResponse(download='path')

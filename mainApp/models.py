22e5b49125ae5c934ab055fe17c064928a7f312b
begimai <begimayapsamatova@gmail.com>
2018-09-04 15:08:41 +0600

    created models for data

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
-class MyModel(models.Model):
+
+class Video(models.Model):
+    email = models.EmailField()
+    path = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+
+
+
+
+
+
+#class Pathes_to_converted_files(models.Model):
+
     # file will be uploaded to MEDIA_ROOT/uploads
     # upload = models.FileField(upload_to='uploads/')
     # or...
     # file will be saved to MEDIA_ROOT/uploads/2015/01/30
-    upload = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    #upload = models.FileField(upload_to='uploads/%Y/%m/%d/')

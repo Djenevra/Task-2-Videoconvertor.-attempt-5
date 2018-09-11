d2ab02b95bd84188417d11c354f1b958ea8d17cd
begimai <begimayapsamatova@gmail.com>
2018-09-11 18:56:22 +0600

    made alterations in names of functions

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import VideoConversionForm
+from .forms import videoconversionform
 from .models import Video
-from .tasks import ConversionAndSendingEmail
+from .tasks import videoconversion
 from django.conf import settings
+from django.core.mail import send_mail
 #recipient = "fortestsonly23@gmail.com"
 #subject = "Ссылка на скачивание Вашей песни"
 #text = "Здесь должна быть ссылка на песню из медиа файла... сик оф зэсе факинг линкс"
@@ -11,20 +12,21 @@ from django.conf import settings
 
 def index(request):
     if request.method == "POST":
-        form = VideoConversionForm(request.POST)
+        form = videoconversionform(request.POST)
 
         if form.is_valid():
             link = form.cleaned_data['links']
-            path, title = ConversionAndSendingEmail(link)
-            email = form.cleaned_data['mail_address']
-            send_mail(subject, text, settings.EMAIL_HOST_USER, [email])
+            url, title = videoconversion(link)
 
-            video = Video(email=email, path=path, title=title)
+            email = form.cleaned_data['mail_address']
+            # send_mail(subject, text, settings.EMAIL_HOST_USER, [email])
+            print(">>>> path ", url)
+            video = Video(email=email, path=url, title=title)
             video.save()
 
-            return HttpResponse("<h1>Thank You. Your download link for {} is /get/{}.</h1> link will be sent to Your email".format(title, video.id))
+            return HttpResponse("<h1>Thank You. Your download link for {} will be sent to Your email".format(title, video.id))
     else:
-        useForm = VideoConversionForm()
+        useForm = videoconversionform()
         return render (request,"mainApp/HomePage.html",{"form":useForm})
 
 

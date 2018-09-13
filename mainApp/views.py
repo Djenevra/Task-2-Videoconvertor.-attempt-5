349a6ebb7cac450219e8f20d2cd5909174c1f480
begimai <begimayapsamatova@gmail.com>
2018-09-13 16:04:53 +0600

    added code to give link to media file

--- mainApp/views.py
+++ mainApp/views.py
@@ -5,10 +5,13 @@ from .models import Video
 from .tasks import videoconversion
 from django.conf import settings
 from django.core.mail import send_mail
+import os
 #recipient = "fortestsonly23@gmail.com"
 #subject = "Ссылка на скачивание Вашей песни"
 #text = "Здесь должна быть ссылка на песню из медиа файла... сик оф зэсе факинг линкс"
 #message = subject+text
+
+# http://127.0.0.1:8000/downloads/mwiQ0IKIlrk/
 
 def index(request):
     if request.method == "POST":
@@ -19,15 +22,28 @@ def index(request):
             url = videoconversion(link)
 
             email = form.cleaned_data['mail_address']
+            get_file = download(request, url)
+
             # send_mail(subject, text, settings.EMAIL_HOST_USER, [email])
-            print(">>>> path ", url)
+            #print(">>>> path ", url)
             video = Video(email=email)
             video.save()
 
-            return HttpResponse("<h1>Thank You. Your download link for will be sent to Your email</h1>")
+            ultimate_url = os.path.join(settings.MEDIA_ROOT, url)
+            print (ultimate_url)
+            if os.path.exists(ultimate_url):
+                with open(ultimate_url, 'rb') as fh:
+                    response = HttpResponse(fh.read(), content_type='application/mp3')
+                    response['Content-Disposition'] = 'attachment; filename = go_fuck_yourself'
+
+                    return response
+
+
+            #return HttpResponse("<h1>Thank You. Your download link will be sent to Your email</h1>")
     else:
         useForm = videoconversionform()
         return render (request,"mainApp/HomePage.html",{"form":useForm})
+
 
 
 #def list(request):
